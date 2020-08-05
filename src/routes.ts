0e45240c70a0e3ace68461844c5be50dd94fe589
arthurmdros <arthurmedeiros2005@gmail.com>
2020-08-05 20:51:07 -0300

    Adicionando rotas do controlador das conexões

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 const classesControllers = new ClassesController();
+const connectionsControlers = new ConnectionsController();
 
 routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
+
+routes.get('/connections', connectionsControlers.index);
+routes.post('/connections', connectionsControlers.create);
 
 export default routes;
